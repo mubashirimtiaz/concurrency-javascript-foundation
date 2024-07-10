54b5513712f6963ada7814d72dd2226cf01a09c2
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2024-07-11 01:43:30 +0500

    updated async.ts

--- async.ts
+++ async.ts
@@ -1,24 +1,30 @@
 function washing(callback) {
+  console.log('washing started...');
   setTimeout(() => {
     console.log('washing done');
     callback();
   }, 5000);
 }
+
 function soak(callback) {
+  console.log('soaking started...');
   setTimeout(() => {
     console.log('soaking done');
     callback();
   }, 3000);
 }
+
 function drying() {
+  console.log('drying started...');
   setTimeout(() => {
     console.log('drying done');
   }, 2000);
 }
+
 washing(() => {
   soak(() => {
     drying();
   });
 });
 
-console.log('I am making Biryani...');
+console.log('I am making Biriyani...');
